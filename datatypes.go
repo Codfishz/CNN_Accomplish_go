84d9c855e7cfc706589b1a20a99a7ddf6a51d4db
C <ray20000309@icloud.com>
2023-11-03 10:21:12 -0400

    Initialization
    Initialization of main, datatypes and train go files

---
+++ datatypes.go
@@ -0,0 +1,7 @@
+package main
+
+type Tensor struct {
+	Data     [][]float32
+	Shape    []int
+	DataType string
+}
---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	//training process
+	fmt.Println("Start training process")
+
+	fmt.Println("End training process")
+
+	//evaluation process
+	fmt.Println("Start evaluation process")
+
+	fmt.Println("End training process")
+
+}
