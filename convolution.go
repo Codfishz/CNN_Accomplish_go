f05c8f5ed80662b99d078b05658c8415b53e2d66
dunhan <dunhan.jiang@outlook.com>
2023-11-03 11:19:06 -0400

    Update convolution.go

--- convolution.go
+++ convolution.go
@@ -1,5 +1,13 @@
 package main
 
+type Tensor struct {
+	Data     [][]float32
+	Shape    []int
+	DataType string
+	Pad int
+	Stride int
+}
+
 func (tensor Tensor) forward(kernalShape []int) Tensor {
 
 	return tensor
