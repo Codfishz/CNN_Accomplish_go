8622766fa57937705266eab49ba3d0dff11832e9
FlynnZ221 <143575162+FlynnZ221@users.noreply.github.com>
2023-11-05 15:52:20 -0500

    Update datatypes.go
    revised Tensor description. Implemented NewTensor function for creating tensor struct with data of dimension b*c*h*w

--- datatypes.go
+++ datatypes.go
@@ -1,8 +1,21 @@
 package main
 
 type Tensor struct {
-	//first dimension for batch size
-	//second dimentsion for channel number
-	//third and fourth dimentsion for width and height
+	// batch size, channel, height, width
 	Data [][][][]float32
+}
+
+// Initialize a tensor with all zeros
+func NewTensor(b, c, h, w int) *Tensor {
+	data := make([][][][]float32, b)
+	for i := 0; i < b; i++ {
+		data[i] = make([][][]float32, c)
+		for j := 0; j < c; j++ {
+			data[i][j] = make([][]float32, h)
+			for k := 0; k < h; k++ {
+				data[i][j][k] = make([]float32, w)
+			}
+		}
+	}
+	return &Tensor{Data: data}
 }
