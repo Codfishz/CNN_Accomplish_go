2d26608c382da24d22469db8d277d2f0336382cd
FlynnZ221 <143575162+FlynnZ221@users.noreply.github.com>
2023-11-20 17:26:45 -0500

    Update main.go

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ func main() {
 	batch_size:=100
 	num_epoch:=10
 	path:="data"
-	k1, b1, k2, b2, w3, b3 = Train(path,learning_rate,num_epoch,batch_size)
+	k1, b1, k2, b2, w3, b3 := Train(path,learning_rate,num_epoch,batch_size)
 
 	//finish training
 	fmt.Println("End training process")
@@ -19,7 +19,7 @@ func main() {
 
 	//evaluation process
 	fmt.Println("Start evaluation process")
-	Accuracy:=Eval(path,k1,b1,k2,b2,w3,b3)
+	Accuracy := eval(path,k1,b1,k2,b2,w3,b3)
 	fmt.Println("Accuracy is ",Accuracy)
 	
 	fmt.Println("End training process")
