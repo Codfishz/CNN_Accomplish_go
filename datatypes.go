f23aaf366e12778e3708345fc38d8ef931a26012
FlynnZ221 <143575162+FlynnZ221@users.noreply.github.com>
2023-11-20 14:41:39 -0500

    implement Reshape in Tensor

--- datatypes.go
+++ datatypes.go
@@ -19,3 +19,32 @@ func NewTensor(b, c, h, w int) *Tensor {
 	}
 	return &Tensor{Data: data}
 }
+
+// Reshape a 4D tensor to a 2D tensor
+func Reshape4Dto2D(matrix4D [][][][]float32) [][]float32 {
+    batchSize := len(matrix4D)
+    if batchSize == 0 {
+        return nil
+    }
+
+    channel := len(matrix4D[0])
+    height := len(matrix4D[0][0])
+    width := len(matrix4D[0][0][0])
+    flatSize := channel * height * width
+
+    matrix2D := make([][]float32, batchSize)
+    for i := range matrix2D {
+        matrix2D[i] = make([]float32, flatSize)
+        flatIndex := 0
+        for j := 0; j < channel; j++ {
+            for k := 0; k < height; k++ {
+                for l := 0; l < width; l++ {
+                    matrix2D[i][flatIndex] = matrix4D[i][j][k][l]
+                    flatIndex++
+                }
+            }
+        }
+    }
+
+    return matrix2D
+}
